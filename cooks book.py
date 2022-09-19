60472dda8798e3ba2c07afe2cc216e81d4f026a2
Alexander Efimenko <itefimchik27@mail.ru>
2022-09-19 21:57:29 +0300

    correction changes and variables

--- cooks book.py
+++ cooks book.py
@@ -1,4 +1,24 @@
 import pprint
+pp = pprint.PrettyPrinter()
+
+def cook_dict():
+    with open('book cooks.txt', encoding='utf-8') as file:
+        cook_book = {}
+        for line in file:
+            data = line.strip().split(' | ')
+
+            if len(data) > 1:
+                ingrident = {'ingridient_name': data[0],
+                             'quantity': float(data[1]),
+                             'measure': data[2]}
+                cook_book[recept].append(ingrident)
+            else:
+                if data[0] == '' or data[0].isdigit():
+                    continue
+                else:
+                    recept = data[0]
+                    cook_book[recept] = []
+    return cook_book
 
 
 def get_shop_list_by_dishes(dishes, person_count):
@@ -6,33 +26,24 @@ def get_shop_list_by_dishes(dishes, person_count):
     result = {}
 
     for dish in dishes:
-        for ingrident in cook_book[dish]:
-            if ingrident['ingridient_name'] in result.keys():
-                result[ingrident['ingridient_name']]['quantity'] += ingrident['quantity'] * person_count
-            else:
-                result[ingrident['ingridient_name']] = {'measure': ingrident['measure'],
-                                                        'quantity': ingrident['quantity'] * person_count}
-    return result
+        if dish in cook_dict().keys():
+            for ingrident in cook_dict()[dish]:
+                if ingrident['ingridient_name'] in result.keys():
+                    result[ingrident['ingridient_name']]['quantity'] += ingrident['quantity'] * person_count
+                else:
+                    result[ingrident['ingridient_name']] = {'measure': ingrident['measure'],
+                                                            'quantity': ingrident['quantity'] * person_count}
+    pp.pprint(result)
 
+def changes():
+    print("название блюда: "
+          "Омлет,"
+          "Утка по-пекински,"
+          "Запеченный картофель,"
+          "Фахитос")
+    dishes = input("Введите название блюда через пробел: ").split()
+    person_count = int(input("Введите количество персон: "))
+    get_shop_list_by_dishes(dishes, person_count)
 
-with open('book cooks.txt', encoding='utf-8') as file:
-    cook_book = {}
-    for line in file:
-        data = line.strip().split(' | ')
+changes()
 
-        if len(data) > 1:
-            ingrident = {'ingridient_name': data[0],
-                         'quantity': float(data[1]),
-                         'measure': data[2]}
-            cook_book[recept].append(ingrident)
-        else:
-            if data[0] == '' or data[0].isdigit():
-                continue
-            else:
-                recept = data[0]
-                cook_book[recept] = []
-
-pp = pprint.PrettyPrinter()
-pp.pprint(cook_book)
-
-pp.pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2))
